962e904c70cdf61dbf61afbba0984a33b59bb78e
bdavez <43177796+bdavez@users.noreply.github.com>
2026-06-27 19:31:47 -0600

    Add files via upload

---
+++ Program2.cpp
@@ -0,0 +1,132 @@
+/* 
+Name: Brendan Davis
+AU ID: 3616762
+Date: 2024-06-28 
+Program: Assignment 1 - Problem 2
+*/
+
+/*
+ DOCUMENTATION
+ 
+ Accreditation: 
+    Found at  https://cplusplus.com/forum/beginner/182128/ for the idea of use cin.clear() 
+    and cin.ignore() to help flush standard input after malformed input to avoid unusal activity.
+    
+ Program Purpose:
+    Converts temperature from Fahrenheit to Celsius and vice versa.
+
+ Classes: none
+
+ Variables:
+        inptDec - input integer from stand input
+        result - integer resulting after temperature conversion
+        inptChar1 - Character to decide whether its Celsius or Fahrenheit conversion
+        inptChar2 - character variable to decide to quit the program
+        inptFail - Character that is stored if malformed input is detected will re-run program
+*/
+
+/*
+ TEST PLAN
+ 
+ Normal case:
+    Please enter your temperature:22
+
+    Please enter the units (F/C):C
+
+    A temperature of 22 degrees Celcius is equivalent to 71 Fahrenheit
+    Do you want another conversion?
+
+ Bad Data case 1 (Character instead of integer input):
+    This program converts Temperatures from Fahrenheit to Celsius and vice versa.
+    Please enter your temperature:ASD
+    Please enter valid input and try again
+    There was an issue with user input, re-running
+    This program converts Temperatures from Fahrenheit to Celsius and vice versa.
+    Please enter your temperature:
+ 
+ Bad Data case 2 (Integer instead of character/string input):
+    This program converts Temperatures from Fahrenheit to Celsius and vice versa.
+    Please enter your temperature:33
+
+    Please enter the units (F/C):123
+
+    Please follow the directions and enter either (C) for celcius or (F) Fahrenheit
+    Do you want another conversion?This program converts Temperatures from Fahrenheit to Celsius and vice versa.
+    Please enter your temperature:Do you want another conversion?
+ 
+ Bad Data case 3 (floating point input)
+    Please enter your temperature:2.2
+    Do you want another conversion?This program converts Temperatures from Fahrenheit to Celsius and vice versa.
+    Please enter your temperature:Do you want another conversion?
+ 
+ Discussion:
+    This program only accepts integer input for temperature
+    This program only accepts character input for Celsius or Fahrenheit
+ 
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <ctype.h>
+
+using namespace std;
+
+int main () 
+{
+    int inptDec, result;
+    char inptChar1, inptChar2, inptFail;
+    string endProg;
+    inptFail = 'y';
+    while (endProg != "quit") {
+        std::cout << "This program converts Temperatures from Fahrenheit to Celsius and vice versa." << endl;
+        std::cout << "Please enter your temperature:" << ends;
+        std::cin >> inptDec;
+
+
+        while (inptFail == 'y') {
+            
+
+            if (cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(100, '\n');
+                std::cout << "Please enter valid input and try again" << endl;
+                inptDec = 0;
+                break;
+            }
+            else {
+                std::cout << endl;
+                std::cout << "Please enter the units (F/C):" << ends;
+                std::cin >> inptChar1;
+                std::cout << endl;
+                if (tolower(inptChar1) == 'c') {
+                    result = (inptDec * 9/5) + 32;
+                    std::cout << "A temperature of " << inptDec << " degrees Celcius is equivalent to " << result << " Fahrenheit" << endl;
+                }
+                else if (tolower(inptChar1) == 'f') {
+                    result = (inptDec - 32) * 5/9;
+                    std::cout << "A temperature of " << inptDec << " degrees Fahrenheit is equivalent to " << result << " Celcius" << endl;
+                }
+                else if (towlower(inptChar1) == 'n') {
+                    endProg = "quit";
+                }
+                else {
+                    std::cout << "Please follow the directions and enter either (C) for celcius or (F) Fahrenheit" << endl;
+                }
+                inptFail = 'n';
+            }
+        }
+        
+        if(inptFail == 'y') {
+            cout << "There was an issue with user input, re-running" << endl;
+            inptChar2 = 'y';
+        }
+        else {
+            std::cout << "Do you want another conversion?";
+            std::cin >> inptChar2;  
+            if (tolower(inptChar2) == 'n') {
+                endProg = "quit";
+            }
+        }   
+    }
+    return 0; 
+}
---
+++ Program4.cpp
@@ -0,0 +1,161 @@
+/* 
+Name: Brendan Davis
+AU ID: 3616762
+Date: 2024-06-28 
+Program: Assignment 1 - Problem 4
+*/
+
+/*
+ DOCUMENTATION
+
+ Program Purpose:
+An interactive help system for C++ programming. 
+The system will incorporate a menu system that interacts with the user, and the appropriate help topic will be displayed, based on an input selection.
+
+ Classes: none
+
+ Variables:
+    opt - Stores a character of the user input
+    qApp - Stores a character value dec
+    usrinpt - Stores a string value of what the user entered
+*/
+
+/*
+ TEST PLAN
+ 
+ Normal case:
+
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+    4
+    while: is a control structure that allows for repetitive execution of a block of code as long as a specified condition is true.
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+
+
+ Bad Data case 1 (Malformed input):
+
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+    @#$#$%!@
+    Please enter a correct input with one character
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+
+ 
+ Bad Data case 2 (Integer instead of character/string input):
+
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+    adasd
+    Please enter a correct input with one character
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+
+ 
+ Bad Data case 3 (floating point input)
+
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+    2.2
+    Please enter a correct input with one character
+    C++ Help Menu
+    Select Help on: 
+    1. If 
+    2. Switch 
+    3. For 
+    4. While 
+    x. Exit
+
+ 
+ Discussion:
+    This program uses input from the user to display help text depending on input
+ 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+    
+    char opt, qApp;
+    string usrinpt;
+    while(qApp != 'q') {
+        cout << "C++ Help Menu\nSelect Help on: \n 1. If \n 2. Switch \n 3. For \n 4. While \n x. Exit" << endl;
+        cin >> usrinpt;
+
+        if(usrinpt.length() > 1) {
+            cout << "Please enter a correct input with one character" << endl;
+        }
+        else {
+            opt = usrinpt[0];
+            switch(opt) {
+            case '1':
+            cout << "if: " << ends;
+            cout << "Is used for conditional execution of a block of code. It is used to execute a block of code if a certain condition is met." << endl;
+            break;
+
+            case '2':
+            cout << "switch: " << ends;
+            cout << " is a multiway branch statement that provides a way to organize the flow of execution to parts of code based on the value of an expression." << endl;
+            break;
+
+            case '3':
+            cout << "for: " << ends;
+            cout << " is defined as a repetition control structure that allows you to efficiently write a loop that needs to execute a specific number of times." << endl;
+            break;
+
+            case '4':
+            cout << "while: " << ends;
+            cout << "is a control structure that allows for repetitive execution of a block of code as long as a specified condition is true." << endl;
+            break;
+
+            case 'x':
+            cout << "exit" << endl;
+            qApp = 'q';
+            break;
+
+            default:
+            cout << "Please enter a correct input from the list of choices" << endl;
+            }
+        }
+    }
+ 
+    return 0;
+}
